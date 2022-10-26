a0f77f4195b36e10287e70bfb57c05e7b7792e93
gloxounet <matteo.glock@laposte.net>
2022-10-26 11:19:15 +0200

    final commit, all docker file done

--- services/auth-api/src/requests/user-api-getter.ts
+++ services/auth-api/src/requests/user-api-getter.ts
@@ -2,6 +2,6 @@ import { getRequest, postRequest } from "./request";
 import { User } from "../model";
 
 export function getUserByName(username: string) {
-	const response = getRequest<User>(`http://127.0.0.1:5000/user/${username}`);
+	const response = getRequest<User>(`http://users-api:5000/user/${username}`);
 	return response;
 }
